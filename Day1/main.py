fd33e1396c08a1d975daa44ff34fb6990aced9da
Özgür Yildirim <ozguryenihayat@gmail.com>
2022-12-01 18:18:57 +0100

    got my first gold star :)

--- Day1/main.py
+++ Day1/main.py
@@ -1,5 +1,22 @@
+"""
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+"""
+
 def main():
-    pass
+    # read calories
+    with open("input.txt", "r") as file:
+        lines = file.readlines()
+    
+    max_elf = 0
+    current_elf = 0
+    for line in lines:
+        if line.isspace():
+            if current_elf > max_elf:
+                max_elf = current_elf
+            current_elf = 0
+        else:
+            current_elf += int(line)
+    print(max_elf)
 
 
 
