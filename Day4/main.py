adb4061188f63ae76881f9566ef2366889cdda44
Özgür Yildirim <ozguryenihayat@gmail.com>
2022-12-04 14:20:36 +0100

    Solve day4 part2

--- Day4/main.py
+++ Day4/main.py
@@ -9,16 +9,29 @@ def lines_generator() -> Iterator[str]:
 
 Range = namedtuple('Range', "begin, end")
 
-def fully_contains(one: Range, other: Range) -> bool:
-    return one.begin <= other.begin and \
-        one.end >= other.end
-
 def ranges_generator():
     for line in lines_generator():
         first, second = line.split(',')
         first = Range(*map(int, first.split('-')))
         second = Range(*map(int, second.split('-')))
         yield first, second
+
+def fully_contains(one: Range, other: Range) -> bool:
+    return one.begin <= other.begin and \
+        one.end >= other.end
+
+def overlap(one: Range, other: Range) -> bool:
+    return one.begin <= other.end and \
+        one.end >= other.begin
+
+
+def get_number_overlapping_pairs() -> int:
+    return sum(
+        1
+        for first, second
+        in ranges_generator()
+        if overlap(first, second)
+    )
 
 
 def get_number_pairs_one_fully_contain_other() -> int:
@@ -31,7 +44,7 @@ def get_number_pairs_one_fully_contain_other() -> int:
     )
 
 def main() -> None:
-    result = get_number_pairs_one_fully_contain_other()
+    result = get_number_overlapping_pairs()
     print(result)
 
 
