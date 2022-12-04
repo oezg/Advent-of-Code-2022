259c52b6e158e1901dc17b0ce74c6e2e66855b91
Özgür Yildirim <ozguryenihayat@gmail.com>
2022-12-04 14:03:19 +0100

    Solve Day4 Part1

--- Day4/main.py
+++ Day4/main.py
@@ -1,13 +1,39 @@
-def lines_generator():
+from collections import namedtuple
+from typing import Iterator
+
+def lines_generator() -> Iterator[str]:
     with open("input.txt", "r") as file:
         for line in file:
             yield line.rstrip()
 
-def main():
-    result = 
+
+Range = namedtuple('Range', "begin, end")
+
+def fully_contains(one: Range, other: Range) -> bool:
+    return one.begin <= other.begin and \
+        one.end >= other.end
+
+def ranges_generator():
+    for line in lines_generator():
+        first, second = line.split(',')
+        first = Range(*map(int, first.split('-')))
+        second = Range(*map(int, second.split('-')))
+        yield first, second
+
+
+def get_number_pairs_one_fully_contain_other() -> int:
+    return sum(
+        1
+        for first, second
+        in ranges_generator()
+        if fully_contains(first, second)
+        or fully_contains(second, first)
+    )
+
+def main() -> None:
+    result = get_number_pairs_one_fully_contain_other()
     print(result)
 
 
 if __name__ == "__main__":
     main()
-
